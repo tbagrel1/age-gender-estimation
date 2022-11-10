0142d6abb089dd6fee2de0e2188571bf7216a948
Thomas BAGREL <tomsb07@gmail.com>
2022-11-10 15:00:53 +0100

    add __init__.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tbagrel1/age-gender-estimation-trcp",
-    packages=setuptools.find_packages(),
+    packages=["age_gender_estimation_trcp"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
