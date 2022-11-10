c6c23dc7297c43904a67898a8c578c7fc8888785
Thomas BAGREL <tomsb07@gmail.com>
2022-11-10 14:15:48 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as req_file:
+    reqs = [l.strip() for l in req_file if l.strip() and not l.strip().startswith("#")]
+
+setuptools.setup(
+    name="age-gender-estimation",
+    version="0.6",
+    author="Yusuke Uchida",
+    author_email="yu4u.github.io",
+    description="Keras implementation of a CNN network for age and gender estimation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tbagrel1/age-gender-estimation",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.5.5',
+    install_requires=reqs
+)
